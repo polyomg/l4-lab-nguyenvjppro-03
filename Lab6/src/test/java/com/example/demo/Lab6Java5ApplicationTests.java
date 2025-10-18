e3db42c93242578d3307347b8ce1d6feba2d4eff
Do Nguyen Nguyen <nguyendohappy@gmail.com>
2025-10-18 15:39:47 +0700

    TS00785 - Lab6

--- Lab6/src/test/java/com/example/demo/Lab6Java5ApplicationTests.java
+++ Lab6/src/test/java/com/example/demo/Lab6Java5ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.poly.lab6;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab6ApplicationTests {
+class Lab6Java5ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
